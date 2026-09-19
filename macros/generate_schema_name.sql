f670b5dda9a0e66ab35865db6bd2c10adbb28c96
asifbrohi@hotmail.co.uk <asifbrohi@hotmail.co.uk>
2026-09-19 22:58:36 +0000

    edited generate schema name sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -2,7 +2,7 @@
     {%- if custom_schema_name is none -%}
         {{ target.schema }}
     {%- else -%}
-        {%- if target.name == 'prd' -%}
+        {%- if target.name == 'prd' or target.name == 'default' or target.name == 'dev' -%}
             {{ custom_schema_name | trim }}
         {%- else -%}
             {{ target.schema ~ '_' ~ custom_schema_name | trim }}
